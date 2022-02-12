1d792330fe54d623bfb93830d25f349d25c55e3e
JeonMoo <rjm0710@naver.com>
2022-02-12 17:38:17 +0900

    Spring boot Gradle Project Setting

---
+++ src/main/java/com/jojoldu/book_real/FreelecSpringboot2WebserviceRealApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book_real;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringboot2WebserviceRealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelecSpringboot2WebserviceRealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jojoldu/book_real/FreelecSpringboot2WebserviceRealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book_real;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringboot2WebserviceRealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
