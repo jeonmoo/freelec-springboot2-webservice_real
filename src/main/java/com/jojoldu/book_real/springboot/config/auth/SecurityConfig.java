f6dde30a4c6ff37b905abf00993555c6ef157402
JeonMoo <rjm0710@naver.com>
2022-03-01 19:27:51 +0900

    Create SecurityConfig

--- src/main/java/com/jojoldu/book_real/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book_real/springboot/config/auth/SecurityConfig.java
@@ -1,17 +1,34 @@
-package com.jojoldu.book_real.springboot.config.auth;
+package com.jounho.springboot.config.auth;
 
+import com.jojoldu.book_real.springboot.domain.user.Role;
 import lombok.RequiredArgsConstructor;
-import org.springframework.
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@EnableWebSecurity //(1)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .headers().frameOptions().disable() //(2)
+                .and()
+                .authorizeRequests() //(3)
+                .antMatchers("/","css/**","/images/**","/js/**","h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name()) //(4)
+                .anyRequest().authenticated() //(5)
+                .and()
+                .logout()
+                .logoutSuccessUrl("/") //(6)
+                .and()
+                .oauth2Login() //(7)
+                .userInfoEndpoint() //(8)
+                .userService(customOAuth2UserService); //(9)
 
+
+    }
 }
